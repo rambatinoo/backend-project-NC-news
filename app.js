018f95b9822bae64472fbe9d869738714ec20d12
rambatinoo <barrywjames1990@gmail.com>
2024-05-28 11:50:01 +0100

    add get request to /api/topics endpoint

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const { getTopics } = require("./controllers/topics-controllers");
+
+app.get("/api/topics", getTopics);
+
+app.use("/", (req, res, next) => {
+  res.status(404).send({ msg: "Non existant endpoint" });
+});
+module.exports = app;
