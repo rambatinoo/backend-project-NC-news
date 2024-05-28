f4897bdddf3c0d33ff25ebb67e9709719817e5ec
rambatinoo <barrywjames1990@gmail.com>
2024-05-28 14:13:37 +0100

    add votes and article_id to the get request for article by Id

--- models/articles-models.js
+++ models/articles-models.js
@@ -1,8 +1,13 @@
 const db = require("../db/connection");
+const format = require("pg-format");
 
 exports.selectArticle = (article_id) => {
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
+    .query(
+      `SELECT * FROM articles 
+    WHERE article_id = $1`,
+      [article_id]
+    )
     .then((result) => {
       return result.rows[0];
     });
