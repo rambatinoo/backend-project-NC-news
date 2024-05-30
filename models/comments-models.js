47c1ef6122ae4bc211637c86830a53b0d6b4cad8
rambatinoo <barrywjames1990@gmail.com>
2024-05-30 10:06:54 +0100

    modified some error handling

--- models/comments-models.js
+++ models/comments-models.js
@@ -26,7 +26,11 @@ exports.addNewComment = (article_id, username, body) => {
 };
 
 exports.removeComment = (comment_id) => {
-  return db.query(`DELETE FROM comments WHERE comment_id = $1`, [comment_id]);
+  return db
+    .query(`DELETE FROM comments WHERE comment_id = $1`, [comment_id])
+    .then(() => {
+      return "deleted";
+    });
 };
 
 exports.selectComment = (comment_id) => {
