e19d4b60fca2a44d0fcfa2990b82542a1b92520e
rambatinoo <barrywjames1990@gmail.com>
2024-05-31 14:06:49 +0100

    moving dependencies for hosting

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
-const config = {};
 
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
+const config = {};
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE not set");
